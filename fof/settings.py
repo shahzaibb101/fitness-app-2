89b60cf5d48623b47f2785bbe798f5dc40c2f3c6
Jahanzaib-baig01 <118760237+Jahanzaib-baig01@users.noreply.github.com>
2024-05-19 19:00:41 +0500

    Update settings.py

--- fof/settings.py
+++ fof/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from home.config import gmail_pass
+import os
+
+# Retrieve secrets from environment variables
+gmail_pass = os.environ.get('GMAIL_PASS')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
